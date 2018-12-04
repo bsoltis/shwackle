fcb188c399f7b3dabc2c93d1dade7a44c9d4a948
Brian Soltis <Brian.Soltis@farragut.com>
2018-12-04 17:20:50 -0500

    Changing menu to semantic ui

--- src/logout.js
+++ src/logout.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import firebase from 'firebase';
+import { Button } from 'semantic-ui-react';
 
 const logOutUser = () => {
     firebase.auth().signOut();
 };
 
 const LogOut = () => {
-    return <a href="#" onClick={logOutUser} className="item">Log Out</a>;
+    return (
+        <Button onClick={logOutUser} inverted>
+            Log Out
+        </Button>
+    );
 };
 
 export default LogOut;
--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Menu, Button, Container } from 'semantic-ui-react';
 
 import Home from './home';
 import Register from './register';
@@ -13,7 +14,33 @@ class Navigation extends Component {
         return (
             <Router>
                 <div>
-                    <div className="ui fixed menu pointing secondary inverted" style={{backgroundColor: '#F15946'}}>
+                    <Menu secondary pointing inverted fixed='top' style={{backgroundColor: '#F15946'}}>
+                        {this.props.authenticated ? (
+                            <Container>
+                                <Menu.Item as={Link} to="/" active>
+                                    Home
+                                </Menu.Item>
+                                <Menu.Item position='right'>
+                                    <LogOut />
+                                </Menu.Item>
+                            </Container>
+                        ) : (
+                            <Container>
+                                <Menu.Item as={Link} to="/" active>
+                                    Home
+                                </Menu.Item>
+                                <Menu.Item position='right'>
+                                    <Button as={Link} to="/login" inverted>
+                                        Log In
+                                    </Button>
+                                    <Button as={Link} to="/register" inverted style={{marginLeft: '0.5em'}}>
+                                        Sign Up
+                                    </Button>
+                                </Menu.Item>
+                            </Container>
+                        )}
+                    </Menu>
+                    {/* <div className="ui fixed menu pointing secondary inverted" style={{backgroundColor: '#F15946'}}>
                         <div className="ui container">
                             <Link to="/" className="active item">Home</Link>
 
@@ -31,7 +58,7 @@ class Navigation extends Component {
                                 </div>
                             )}
                         </div>
-                    </div>
+                    </div> */}
                     <Switch>
                         <Route exact path="/" component={Home} />
                         <Route authenticated={this.props.authenticated} path="/login" component={Login} />
