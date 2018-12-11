779137786df193640b5a2b47475b533eacaed5ae
Brian Soltis <Brian@Reginas-MacBook-Pro.local>
2018-12-11 15:45:54 -0500

    delete project and toast

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { withFirebase } from './Firebase';
 import { AuthUserContext } from './Session';
 import { withAuthentication } from './Session';
 import DeleteProjectModal from './deleteproject';
-import { Card, Icon, Container, Header, Modal, Input, Button, Dropdown } from 'semantic-ui-react';
+import { Card, Icon, Container, Header, Modal, Grid, Button, Form, Popup } from 'semantic-ui-react';
 
 class Dashboard extends Component {
     render() {
@@ -26,6 +25,7 @@ class DashboardBase extends Component {
             addProjectModalOpen: false,
             deleteProjectModalOpen: false,
             projectName: '',
+            deleteProjectId: null,
         }
     }
 
@@ -49,10 +49,9 @@ class DashboardBase extends Component {
     }
 
     addNewProject = (event) => {
-        this.setState(
-            {addProjectModalOpen: true},
-            () => this.ref.focus()
-        );
+        this.setState({
+            addProjectModalOpen: true
+        });
     }
 
     handleAddProjectModalClose = () => {
@@ -81,68 +80,88 @@ class DashboardBase extends Component {
         });
     }
 
-    handleRef = component => (this.ref = component);
-
-    handleDeleteProjectModalOpen = () => {
+    handleDeleteProjectModalOpen = (id) => {
         this.setState({
             deleteProjectModalOpen: true,
+            deleteProjectId: id,
+        });
+    }
+
+    handleDeleteProjectModalClose = () => {
+        this.setState({
+            deleteProjectModalOpen: false,
         });
     }
 
     render() {
         const projects = this.state.projects;
-
         const modalDisabled = this.state.projectName === '';
 
         return (
-                <Container style={{marginTop: '7em'}}>
-                    <Header as="h1" inverted>
-                        Welcome to Shwackle
-                    </Header>
-                    <Header as="h3" inverted>
-                        Manage existing projects or create a new project.
-                    </Header>
-                    <Card.Group itemsPerRow={4} className='project-cards' stackable>
-                        <Card color='red' onClick={this.addNewProject}>
+            <Container style={{marginTop: '7em'}}>
+                <Header as="h1" inverted>
+                    Welcome to Shwackle
+                </Header>
+                <Header as="h3" inverted>
+                    Manage existing projects or create a new project.
+                </Header>
+                <Card.Group itemsPerRow={4} className='project-cards' stackable>
+                    <Card color='red' onClick={this.addNewProject}>
+                        <Card.Content textAlign="center">
+                            <Icon name="plus circle" size="huge" style={{margin: '0.6em 0em'}}/>
+                            <Card.Header>Add a project</Card.Header>
+                            <Card.Description>Click here to add a new SCRUM project.</Card.Description>
+                        </Card.Content>
+                    </Card>
+                    {projects.map(project => (
+                        <Card color='red' key={project.id}>
                             <Card.Content textAlign="center">
-                                <Icon name="plus circle" size="huge" style={{margin: '0.6em 0em'}}/>
-                                <Card.Header>Add a project</Card.Header>
-                                <Card.Description>Click here to add a new SCRUM project.</Card.Description>
+                                <Card.Header>{project.data.name}</Card.Header>
+                                <Card.Description>
+                                <Grid columns={2}>
+                                    <Grid.Row>
+                                        <Grid.Column>
+                                            <Popup trigger={<Button color='green' icon='dashboard' basic fluid size='massive'/>} content='Dashboard' />
+                                        </Grid.Column>
+                                        <Grid.Column>
+                                            <Popup trigger={<Button color='blue' icon='th' basic fluid size='massive'/>} content='Boards' />
+                                        </Grid.Column>
+                                    </Grid.Row>
+
+                                    <Grid.Row>
+                                        <Grid.Column>
+                                            <Popup trigger={<Button color='red' icon='pie graph' basic fluid size='massive'/>} content='Analytics' />
+                                        </Grid.Column>
+                                        <Grid.Column>
+                                            <Popup trigger={<Button color='yellow' icon='users' basic fluid size='massive'/>} content='Manage Users' />
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
+                                </Card.Description>
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Button icon='trash' basic style={{float: 'right'}} onClick={() => this.handleDeleteProjectModalOpen(project.id)}/>
+                                <Button icon='setting' basic style={{float: 'right'}} />
                             </Card.Content>
                         </Card>
-                        {projects.map(project => (
-                            <Card color='red' key={project.id}>
-                                <Card.Content textAlign="center">
-                                    <Card.Header>{project.data.name}</Card.Header>
-                                    <Card.Description></Card.Description>
-                                </Card.Content>
-                                <Card.Content extra>
-                                    <Dropdown text='Options' style={{float: 'right'}}>
-                                        <Dropdown.Menu>
-                                            <Dropdown.Item icon='tag' text='Rename' />
-                                            <Dropdown.Item icon='trash' text='Delete' onClick={this.handleDeleteProjectModalOpen} />
-                                        </Dropdown.Menu>
-                                    </Dropdown>
-                                </Card.Content>
-                            </Card>
-                        ))}
-                    </Card.Group>
-                    <Modal open={this.state.addProjectModalOpen} size='small' onClose={this.handleAddProjectModalClose}>
-                        <Modal.Header>Add a new project</Modal.Header>
-                        <Modal.Content>
-                            <Input placeholder="Project Name" fluid onChange={this.handleProjectNameChange} name='projectName' ref={this.handleRef} />
-                        </Modal.Content>
-                        <Modal.Actions>
-                            <Button color='green' onClick={this.handleCreateProjectClick} disabled={modalDisabled}>
-                                <Icon name='checkmark' /> Create
-                            </Button>
-                        </Modal.Actions>
-                    </Modal>
-                    <DeleteProjectModal modalOpen={this.state.deleteProjectModalOpen} />
-                </Container>
-
+                    ))}
+                </Card.Group>
+                <Modal open={this.state.addProjectModalOpen} size='small' onClose={this.handleAddProjectModalClose}>
+                    <Modal.Header>Add a new project</Modal.Header>
+                    <Modal.Content>
+                        <Form onSubmit={this.handleCreateProjectClick} id='addProjectForm'>
+                            <Form.Input placeholder="Project Name" fluid onChange={this.handleProjectNameChange} name='projectName' autoFocus />
+                        </Form>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button type='submit' form='addProjectForm' color='green' disabled={modalDisabled}>
+                            <Icon name='checkmark' /> Create
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
+                <DeleteProjectModal modalOpen={this.state.deleteProjectModalOpen} projectId={this.state.deleteProjectId} onClose={this.handleDeleteProjectModalClose} />
+            </Container>
         );
     }
-
 }
 export default withAuthentication(Dashboard);
--- src/deleteproject.js
+++ src/deleteproject.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Modal, Input, Button, Icon } from 'semantic-ui-react';
+import { withFirebase } from './Firebase';
+import { Modal, Button, Icon, Form } from 'semantic-ui-react';
+import { SemanticToastContainer, toast } from 'react-semantic-toasts';
 
 class DeleteProjectModal extends Component {
     constructor(props) {
@@ -8,13 +10,16 @@ class DeleteProjectModal extends Component {
         this.state = {
             deleteConfirmation: '',
             modalOpen: this.props.modalOpen,
+            projectId: this.props.projectId,
         }
     }
 
     componentWillReceiveProps(nextProps) {
         if (this.props != nextProps) {
             this.setState({
+                deleteConfirmation: '',
                 modalOpen: nextProps.modalOpen,
+                projectId: nextProps.projectId,
             });
         }
     }
@@ -23,43 +28,52 @@ class DeleteProjectModal extends Component {
         this.setState({ [event.target.name]: event.target.value });
     }
 
-    handleOpen = () => {
-        this.setState({}, () => this.ref.focus() );
-    }
+    deleteProject = (event) => {
+        event.preventDefault();
+        const projectId = this.state.projectId;
+        this.props.onClose();
 
-    handleClose = () => {
-        this.setState({
-            modalOpen: false,
+        this.props.firebase.store.collection("projects").doc(projectId).delete().then(() => {
+            toast({
+                type: 'success',
+                description: 'Project deleted',
+                time: 2000
+            });
+        }).catch(function(error) {
+            toast({
+                type: 'success',
+                description: 'Error deleting project',
+                time: 2000
+            });
         });
     }
 
-    deleteProject = () => {
-
-    }
-
-    handleRef = component => (this.ref = component);
-
     render() {
-        const isInvalid = this.state.deleteConfirmation != 'DELETE';
+        const isInvalid = this.state.deleteConfirmation !== 'DELETE';
 
         return (
-            <Modal open={this.state.modalOpen} size='small' onClose={this.handleClose} onOpen={this.handleOpen} basic>
-                <Modal.Header>Are you sure you want to delete?</Modal.Header>
-                <Modal.Content>
-                    <p>This cannot be undone. Type DELETE below and click Delete if you want to proceed.</p>
-                    <Input placeholder="DELETE" fluid onChange={this.handleChange} name='deleteConfirmation' ref={this.handleRef} />
-                </Modal.Content>
-                <Modal.Actions>
-                    <Button color='grey' onClick={this.handleClose}>
-                        <Icon name='cancel' inverted/> Cancel
-                    </Button>
-                    <Button color='red' onClick={this.deleteProject} disabled={isInvalid}>
-                        <Icon name='trash' /> Delete
-                    </Button>
-                </Modal.Actions>
-            </Modal>
+            <div>
+                <Modal open={this.state.modalOpen} size='small' onClose={this.props.onClose} basic>
+                    <Modal.Header>Are you sure you want to delete?</Modal.Header>
+                    <Modal.Content>
+                        <Form onSubmit={this.deleteProject} id='deleteProjectForm'>
+                            <p>This cannot be undone. Type DELETE below and click Delete if you want to proceed.</p>
+                            <Form.Input placeholder="DELETE" fluid onChange={this.handleChange} name='deleteConfirmation' autoFocus />
+                        </Form>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color='grey' onClick={this.props.onClose}>
+                            <Icon name='cancel' inverted/> Cancel
+                        </Button>
+                        <Button type='submit' form='deleteProjectForm' color='red' disabled={isInvalid}>
+                            <Icon name='trash' /> Delete
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
+                <SemanticToastContainer position='bottom-right' />
+            </div>
         );
     }
 }
 
-export default DeleteProjectModal;
+export default withFirebase(DeleteProjectModal);
